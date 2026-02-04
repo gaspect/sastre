efa46b07b5e1e56eb8e1364bfa2e0a85e5623382
Jesús Enrique Fuentes González <jesusefg12@gmail.com>
2026-02-03 23:15:19 -0500

    Update project metadata and expand exports: refine pyproject details, add new dependencies, and expose additional modules in `__init__.py`.

--- sastre/__init__.py
+++ sastre/__init__.py
@@ -1,3 +1,12 @@
 from .renderer import Renderer
+from .scaffold import Scaffold
+from .manager import ExtensionManager
+from sastre.extensions import (
+    Extension, BaseExtension, Htmx, HtmxHelper, Tailwind, 
+    Alpine, React, Svelte, Lucide, Vue
+)
 
-__all__ = ["Renderer"]
+__all__ = [
+    "Renderer", "Scaffold", "ExtensionManager", "Extension", "BaseExtension", 
+    "Htmx", "HtmxHelper", "Tailwind", "Alpine", "React", "Svelte", "Lucide", "Vue"
+]
